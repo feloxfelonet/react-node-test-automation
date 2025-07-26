eac1888a77ac343897a5e5f02528c23930c77ae4
feloxfelonet <46874030+feloxfelonet@users.noreply.github.com>
2025-07-26 17:47:33 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App = () => <h1>Todo App</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
