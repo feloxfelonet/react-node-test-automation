340765dc56112724ea570fa164c4cd510bcf2f60
feloxfelonet <46874030+feloxfelonet@users.noreply.github.com>
2025-07-26 17:47:17 +0100

    Add files via upload

---
+++ client_index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Todo App</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
